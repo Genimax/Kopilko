b953f2cf8fbd74b6d97071b42201d4f5cc2bc253
Genimax <urdnot.wrexx21@gmail.com>
2022-09-11 21:27:24 +0700

    frontend with animated password display icons

--- backend/public/scripts/registrationScript.js
+++ backend/public/scripts/registrationScript.js
@@ -17,6 +17,25 @@ function passwordVisibility(password) {
   }
 }
 
+function passwordVisibility(password) {
+  if (password) password = `-${password}`;
+  const passwordAuthInput = document.getElementById(
+    `password-auth-input${password ? password : ''}`
+  );
+  const hideIcon = document.getElementById(
+    `icon-hide-password${password ? password : ''}`
+  );
+  const hideIconWhite = document.getElementById(
+    `icon-hide-password-white${password ? password : ''}`
+  );
+
+  if (passwordAuthInput.type === 'password') {
+    passwordAuthInput.type = 'text';
+  } else {
+    passwordAuthInput.type = 'password';
+  }
+}
+
 function errorSwitch() {
   const loginField = document.getElementById('login-auth-input');
   const passwordField = document.getElementById('password-auth-input');
